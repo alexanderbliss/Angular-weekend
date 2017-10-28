079940afdfa8588dfb6527d0c4d55705956bb7c4
Alexander Bliss <alexanderbliss@Alexanders-MacBook-Pro.local>
2017-10-27 20:06:08 -0500

    added pics and a like button.

--- server/public/client.js
+++ server/public/client.js
@@ -1,1 +1,20 @@
 var instaApp = angular.module('instaApp', []);
+
+instaApp.controller('instaController', function (){
+    
+    var insta = this;
+
+
+
+
+
+
+var pics = [{ name: 'murray', photo: "http://www.fillmurray.com/300/300"},
+    { name: 'murray2', photo: "http://www.fillmurray.com/400/400}"}
+];
+
+    insta.pics = pics;
+
+    console.log(pics);
+    
+});
