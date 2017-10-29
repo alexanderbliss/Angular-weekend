f7623497b29eac127c8fc964ae9b0ae01a5962e0
Alexander Bliss <alexanderbliss@Alexanders-MacBook-Pro.local>
2017-10-29 17:17:45 -0500

    added description veiw and veiw counter, but not wroking yet.

--- server/public/client.js
+++ server/public/client.js
@@ -1,20 +1,27 @@
 var instaApp = angular.module('instaApp', []);
 
-instaApp.controller('instaController', function (){
-    
+instaApp.controller('instaController', function () {
+
     var insta = this;
+    insta.veiw = 0
+    insta.messge = ""
+    var pics = [{ name: 'My dog Maeby stealing an eggroll', photo: "https://i.imgur.com/Pnc6IDt.jpg?1" },
+    { name: 'My cat alley being allergic to the sun', photo: "https://i.imgur.com/Vic123h.jpg?1" },
+    { name: 'Christmas 2010', photo: 'https://i.imgur.com/RVFXqCe.jpg?1' },
+    { name: 'Me and my brothers resting during a long hike', photo: 'https://i.imgur.com/adjat3s.jpg?1' },
+    { name: 'Cats in a bag', photo: 'https://i.imgur.com/KgeOEGO.jpg?1' },
+    { name: 'Me and my girlfriend at my brothers wedding', photo: 'https://i.imgur.com/LfC2TPv.jpg?1' },
+    ];
+    insta.showDes = function (data) {
+        insta.message = data.name
+        insta.veiw +=1;
+        console.log(insta.veiw);
+        
+        console.log('click');
 
-
-
-
-
-
-var pics = [{ name: 'murray', photo: "http://www.fillmurray.com/300/300"},
-    { name: 'murray2', photo: "http://www.fillmurray.com/400/400}"}
-];
-
+    }
     insta.pics = pics;
 
     console.log(pics);
-    
+
 });
